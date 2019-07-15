a145b49056d780dfe65de9faf173f471465a29aa
karukuma <36288167+karukuma@users.noreply.github.com>
2019-07-15 23:45:39 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -18,18 +18,5 @@ async def ping(ctx):
 async def neko(ctx):
     await ctx.send('にゃーおーん')
     
-client = discord.Client()
-
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-       
+      
 bot.run(token)
